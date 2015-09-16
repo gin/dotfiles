4fd2cf4db8b0d9af5756ab9996cc8b5c20dce530
Luigi Leung <luigi@alumni.cmu.edu>
2015-09-15 22:27:59 -0700

    Add install.sh for OSX

---
+++ install.sh
@@ -0,0 +1,13 @@
+brew install docker
+brew install docker-machine
+
+brew install nvm
+mkdir ~/.nvm
+cp $(brew --prefix nvm)/nvm-exec ~/.nvm/
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+brew install tmux
+brew install htop-osx
+brew install tig
+
